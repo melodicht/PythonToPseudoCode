c1083f437e7bf6f91b00845b7a3b647c6137a295
Marvin Chong <marvincht@gmail.com>
2020-03-15 21:33:06 +0800

    Finalized __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
 from test_cases import PseudocodeConverter
 
-# filename = input("What is the name of the file? ")
-filename = "test 3"
+filename = input("What is the name of the file? ")
 
 line_series = [line.rstrip('\n') for line in open(filename + '.py')]
 
-print(line_series)
 converter = PseudocodeConverter(line_series)
 converted_lines = converter.get_converted_lines()
 
